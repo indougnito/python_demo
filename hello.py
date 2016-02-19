5c20b83c6f18e2f4d019767d66c2654df583dc74
Douglas Hohensee <dhohenseeosx@Douglass-MacBook-Pro.local>
2016-02-19 13:31:32 -0500

    add hello script

---
+++ hello.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+'''
+hello.py
+Prints a hello message to stdout.
+For help, try invoking with the '-h' flag.
+'''
+import argparse
+
+def someMethod(args):
+  print('Here are the arguments:')
+  print(args)
+
+if __name__ == '__main__':
+  # parse arguments
+  parser = argparse.ArgumentParser(description=__doc__,
+                                   formatter_class=argparse.RawDescriptionHelpFormatter)
+  parser.add_argument('-a', '--apple', type=str, default='granny smith',
+                      help='the apple parameter')
+  parser.add_argument('-b', '--banana', type=str, default='cavendish',
+                      help='the banana parameter')
+  args = parser.parse_args()
+  # say hello
+  print('Hello, world')
+  # execute a method
+  someMethod(args)
+  # say goodbye
+  print('Goodnight, world')
